4c0490486cc7571d3a6491bdd7653bd1e038cda2
zoolpher <tmdoom48@gmail.com>
2026-07-20 21:22:57 +0530

    [add] new field to command.hpp

--- include/command.hpp
+++ include/command.hpp
@@ -11,12 +11,14 @@ using std::vector;
 struct Command {
     vector<string> args;
 
-    string output   =  "";
-    string input    = "";
+    string output       =  "";
+    string input_file   = "";
 
-    bool append     = false;
-    bool overWrite  = false;
-    bool bgFlag     = false;
+    bool append         = false;
+    bool overWrite      = false;
+    bool bgFlag         = false;
+    
+    bool runOnlyIfPrevSucceeded    = false;    // [FIX] Bug 3 in src/parser.cpp
 };
 
 
